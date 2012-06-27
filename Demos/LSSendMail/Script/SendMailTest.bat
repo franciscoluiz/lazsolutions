b8bc566e391fa4e182608e53c5961f43f13ecead
Silvio Clecio <silvioprog@gmail.com>
2012-06-27 19:27:08 -0300

    ----

---
+++ Demos/LSSendMail/Script/SendMailTest.bat
@@ -0,0 +1,1 @@
+lssendmail.exe -from="Your name" -to="destination@gmail.com" -subject="Your subject." -message="Your message." -user="example@gmail.com" -password="abcd1234" -host="smtp.gmail.com" -port="465" -ssl="y" -tls="y"
